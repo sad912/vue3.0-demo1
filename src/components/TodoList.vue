e895f63c2273b5ebc32d15a57fb82acf11155393
sad.wang <sad912@foxmail.com>
2022-06-06 14:33:27 +0800

    feat:composition API and setup script demo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { ref } from 'vue'
+
+  let title = ref('')
+  let todoList = ref([
+    {
+      title: '学习 Vue 3',
+      done: false,
+    },
+  ])
+  function addTodo() {
+    todoList.value.push({
+      title: title.value,
+      done: false,
+    })
+    title.value = ''
+  }
+</script>
+
+<template>
+  <input v-model="title" type="text" @keydown.enter="addTodo" />
+  <ul v-if="todoList.length">
+    <li v-for="todoItem in todoList" :key="todoItem.title">
+      <input v-model="todoList.done" type="checkbox" />
+      <span :class="{ done: todoList.done }">{{ todoItem.title }}</span>
+    </li>
+  </ul>
+</template>
--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,3 +1,8 @@
+<script setup>
+  import TodoList from '../components/TodoList.vue'
+</script>
+
 <template>
   <h1>这是首页</h1>
+  <TodoList />
 </template>
